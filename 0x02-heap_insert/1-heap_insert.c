70d07fd20afbc6ec64f3c8b73590cb1cedf4e450
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-05-07 05:01:35 +0000

    1-heap

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -3,7 +3,6 @@
  * treeHeight- Height of bt
  * @tmp: Pointer to tmp
  * Return: number
- * 
  */
 int treeHeight(heap_t *tmp)
 {
@@ -34,49 +33,56 @@ int treeHeight(heap_t *tmp)
  * @tmp: tmp var
  * @height: height of bt
  * Return: returns node of binary tree
- * 
+ *
  */
 
 heap_t getTheLastNode(heap_t *tmp, int height)
 {
-    
-	if(height == 1){
-        return(*tmp);
-    }
-    else if (height > 1){
-        getTheLastNode(tmp->left, height--);
-        getTheLastNode(tmp->right, height--);
-    }
-    return(*tmp);
+	if (height == 1)
+	{
+		return (*tmp);
+	}
+	else if (height > 1)
+	{
+		getTheLastNode(tmp->left, height--);
+		getTheLastNode(tmp->right, height--);
+	}
+	return (*tmp);
 }
+/**
+ * heap_insert - Heaps Insert Fucntion
+ * @root: root of the bt
+ * @value: Value of the bt
+ * Return: Return a heat max bt
+ */
 
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *newnode;
+	heap_t *tmp = *root;
+	int height = 0;
 
-
-heap_t *heap_insert(heap_t **root, int value){
-    heap_t *newnode;
-    heap_t *tmp = *root;
-    int height = 0;
-
-    newnode = malloc(sizeof(heap_t));
-    if (newnode == NULL)
-    {
-        return (NULL);
-    }
-    newnode->n = value;
-    if (*root == NULL){
-        *root = newnode;
-        newnode->parent = NULL;
+	newnode = malloc(sizeof(heap_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = value;
+	if (*root == NULL)
+	{
+		*root = newnode;
+		newnode->parent = NULL;
 		newnode->left = NULL;
 		newnode->right = NULL;
-    }
-    else{
-        height = treeHeight(tmp);
-        *tmp = getTheLastNode(tmp, height);
-        tmp->left = newnode;
-        newnode->parent = tmp;
-        newnode->left = NULL;
-        newnode->right = NULL;
-    }
-    return(newnode);
-    
+	}
+	else
+	{
+		height = treeHeight(tmp);
+		*tmp = getTheLastNode(tmp, height);
+		tmp->left = newnode;
+		newnode->parent = tmp;
+		newnode->left = NULL;
+		newnode->right = NULL;
+	}
+	return (newnode);
 }
