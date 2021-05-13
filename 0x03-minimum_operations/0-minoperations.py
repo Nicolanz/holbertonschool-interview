e2619d0cffb5c23dfe7c1c942179a5bc08762e4c
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-05-13 19:09:14 +0000

    O min

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -33,22 +33,23 @@ def minOperations(n):
     Returns:
         [int]: [Number of operations]
     """
-    if type(n) is not int and n <= 1:
-        return 0
-
     primeList = []
     result = 0
-    number = n
 
-    for i in range(2, n + 1):
+    if n <= 1:
+        return 0
+    elif checkIfPrime(n) is True:
+        return n
+
+    for i in range(2, n):
         if checkIfPrime(i) is True:
             primeList.append(i)
 
     i = 0
 
-    while number != 1:
-        if number % primeList[i] == 0:
-            number = number // primeList[i]
+    while n > 1:
+        if n % primeList[i] == 0:
+            n = n / primeList[i]
             result += primeList[i]
         else:
             i += 1
