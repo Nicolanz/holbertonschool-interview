1bbe5afe71b32152429dc8d9ee26e507b85cb683
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-05-13 18:54:57 +0000

    0-min

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,41 +4,43 @@
 
 def checkIfPrime(n):
     """Checks if a number is prime or not
-
     Args:
         n ([int]): [Numbet to compare primes]
-
     Returns:
         [bool]: [True if prime or false if false]
     """
-    if n > 1:
-        for i in range(2, n):
-            if (n % i) == 0:
-                return False
-        return True
-    else:
+    if (n <= 1):
         return False
+    if (n <= 3):
+        return True
+
+    if (n % 2 == 0 or n % 3 == 0):
+        return False
+
+    i = 5
+    while(i * i <= n):
+        if (n % i == 0 or n % (i + 2) == 0):
+            return False
+        i = i + 6
+
+    return True
 
 
 def minOperations(n):
     """Funtions tp get the min needed operations to get a result
-
     Args:
         n ([int]): [Number to evaluate]
-
     Returns:
         [int]: [Number of operations]
     """
-    if type(n) != int and n <= 1:
+    if type(n) is not int and n <= 1:
         return 0
-    elif (checkIfPrime(n) is True):
-        return n
 
     primeList = []
     result = 0
     number = n
 
-    for i in range(0, n):
+    for i in range(2, n + 1):
         if checkIfPrime(i) is True:
             primeList.append(i)
 
@@ -47,7 +49,7 @@ def minOperations(n):
     while number != 1:
         if number % primeList[i] == 0:
             number = number // primeList[i]
-            result = result + primeList[i]
-        if number % primeList[i] != 0:
-            i = i + 1
+            result += primeList[i]
+        else:
+            i += 1
     return result
