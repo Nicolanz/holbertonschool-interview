176e7d2071b47986b5d2ebc4343e573c2ebe3205
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-05-13 20:32:23 +0000

    greater than 1

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -13,7 +13,7 @@ def minOperations(n):
     newList = []
 
     if n <= 1:
-        return 0
+        return result
 
     for i in range(2, n + 1):
         if n % i == 0:
@@ -21,7 +21,7 @@ def minOperations(n):
 
     i = 0
 
-    while (n != 1):
+    while (n > 1):
         if n % newList[i] == 0:
             n = n / newList[i]
             result += newList[i]
