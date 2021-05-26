6bede22729f424e3a7670f9a5f408aa92aff2623
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-05-26 23:58:59 +0000

    0-is_palimdrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,24 +1,5 @@
 #include "lists.h"
 /**
- * addNode - Adds node to the head of the linked list
- * @new: Pointer to the new linked list
- * @n: value of the node
- * Return: The new node
- */
-listint_t *addNode(listint_t **new, const int n)
-{
-	listint_t *newnode;
-
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
-		return (NULL);
-
-	newnode->n = n;
-	newnode->next = *new;
-	*new = newnode;
-	return (newnode);
-}
-/**
  * is_palindrome - Checks if a linked list is palindrome
  * @head: pointer to the head node
  * Return: True if palindorme or false if not palindrome
@@ -26,43 +7,29 @@ listint_t *addNode(listint_t **new, const int n)
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp = *head;
-	listint_t *new = NULL;
-	listint_t *tmp_two, *new_two;
+	int i = 0;
+	int arr[10000];
 
-	if (tmp == NULL)
+	if (*head == NULL)
 	{
 		return (0);
 	}
-	else if (tmp->next == NULL)
+	while (tmp != NULL)
 	{
-		return (1);
-	}
-	while (tmp->next != NULL)
-	{
-		addNode(&new, tmp->n);
+		arr[i] = tmp->n;
 		tmp = tmp->next;
-		if (tmp->next == NULL)
-		{
-			addNode(&new, tmp->n);
-		}
+		i++;
 	}
-	tmp_two = *head;
-	new_two = new;
-	while (new_two->next != NULL && tmp_two->next != NULL)
+	tmp = *head;
+	i--;
+	while (i >= 0)
 	{
-		if (new_two->n != tmp_two->n)
+		if (tmp->n != arr[i])
 		{
 			return (0);
 		}
-		new_two = new_two->next;
-		tmp_two = tmp_two->next;
-		if (new_two->next == NULL && tmp_two->next == NULL)
-		{
-			if (new_two->n != tmp_two->n)
-			{
-				return (0);
-			}
-		}
+		tmp = tmp->next;
+		i--;
 	}
 	return (1);
 }
