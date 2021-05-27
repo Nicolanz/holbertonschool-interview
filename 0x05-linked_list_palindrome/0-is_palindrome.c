caa849f80ac4a3f1e38d28d5fed9658ab5753b15
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-05-27 00:45:44 +0000

    Try

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -7,7 +7,7 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp = *head;
-	int i = 0;
+	int i, j = 0;
 	int arr[10000];
 
 	if (*head == NULL)
@@ -22,15 +22,12 @@ int is_palindrome(listint_t **head)
 	}
 	tmp = *head;
 	i--;
-	while (i >= 0)
+	j = i;
+	while (i >= j / 2)
 	{
 		if (tmp->n != arr[i])
 		{
 			return (0);
-		}
-		else if (tmp->next == NULL)
-		{
-			return (1);
 		}
 		tmp = tmp->next;
 		i--;
