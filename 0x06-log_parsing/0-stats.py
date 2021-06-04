bfbc6f1ba2de53f1bb14da7fd12733979a56c510
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-06-04 00:39:56 +0000

    0-stats

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,71 +1,40 @@
 #!/usr/bin/python3
-
-from datetime import datetime
+"""Module that reads stdin line by line and computes
+"""
 import sys
-import signal
 
+status_list = [200, 301, 400, 401, 403, 404, 405, 500]
+new_list = []
+suma = 0
+counter = 0
 
-def validateData(x):
-    for i in range(len(x)):
-        if i == 0:
-            tmp = x[i].split(".")
-            for num in tmp:
-                if int(num) <= 0 or int(num) > 255:
-                    return False
-        elif i == 1:
-            try:
-                x[i] = datetime.strptime(x[i], '%Y-%m-%d %H:%M:%S.%f')
-            except:
-                return False
-        elif i == 2:
-            if x[i] != 'GET /projects/260 HTTP/1.1':
-                print(x[i])
-                return False
-        elif i == 3:
-            if int(x[i]) not in [200, 301, 400, 401, 403, 404, 405, 500]:
-                return False
-            x[i] = int(x[i])
-        elif i == 4:
-            try:
-                x[i] = int(x[i])
-            except:
-                return False
-    return True
+try:
+    for line in sys.stdin:
+        tmp_list = line.split()
 
+        if(len(tmp_list) < 2):
+            continue
 
-def printResults(list, sum):
-    print("File size: {}".format(sum))
-    list = sorted(list)
-    for j in range(len(list)):
-        if list[j] != list[j - 1]:
-            print("{:d}: {:d}".format(list[j], list.count(list[j])))
+        suma += int(tmp_list[len(tmp_list) - 1])
+        counter += 1
+        new_list.append(int(tmp_list[len(tmp_list) - 2]))
 
+        if (counter == 10):
+            print("File size: {}".format(suma))
+            new_list = sorted(new_list)
 
-def sigint_handler(signal, frame):
-    printResults(list, sum)
-    sys.exit(0)
-
-
-list = []
-sum = 0
-signal.signal(signal.SIGINT, sigint_handler)
-
-for line in sys.stdin:
-    x = line.split()
-    x[2] += " " + x[3]
-    x[4] += " " + x[5]
-    x[4] += " " + x[6]
-    del x[1]
-    del x[2]
-    del x[3]
-    del x[3]
-    x[1] = x[1].strip("[]")
-    x[2] = x[2].strip('"')
-
-    if validateData(x) is True:
-        sum += x[4]
-        list.append(x[3])
-    if(len(list) == 10):
-        printResults(list, sum)
-        sum = 0
-        list = []
+            for i in range(len(new_list)):
+                if new_list[i - 1] != new_list[i]:
+                    print("{}: {}".format(
+                        new_list[i],
+                        new_list.count(new_list[i])
+                        ))
+            counter = 0
+except:
+    print("File size: {}".format(suma))
+    for i in range(len(new_list)):
+                if new_list[i - 1] != new_list[i]:
+                    print("{}: {}".format(
+                        new_list[i],
+                        new_list.count(new_list[i])
+                        ))
