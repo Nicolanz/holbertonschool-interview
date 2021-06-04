19048fd98a864088c56be257389f897a16045148
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-06-04 03:13:15 +0000

    Task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,6 +2,44 @@
 """Module that reads stdin line by line and computes
 """
 import sys
+from datetime import datetime
+
+
+def validateData(tmp_list):
+    """Function to validate given data
+
+    Args:
+        x ([list]): [Listo of arguments]
+
+    Returns:
+        [bool]: [True id valid data or false if invalid data]
+    """
+    tmp = tmp_list.strip("[]")
+    tmp = tmp.split()
+    del tmp[1]
+    tmp[1] += " " + tmp[2]
+    del tmp[2]
+    tmp[1] = tmp[1].strip("[]")
+    tmp[2] += " " + tmp[3] + " " + tmp[4]
+    tmp[2] = tmp[2].strip('"')
+    del tmp[3]
+    del tmp[3]
+
+    for i in range(len(tmp)):
+        if i == 0:
+            num_tmp = tmp[i].split(".")
+            for num in num_tmp:
+                if int(num) <= 0 or int(num) > 255:
+                    return False
+        elif i == 1:
+            try:
+                tmp[i] = datetime.strptime(tmp[i], '%Y-%m-%d %H:%M:%S.%f')
+            except:
+                return False
+        elif i == 2:
+            if tmp[i] != 'GET /projects/260 HTTP/1.1':
+                return False
+    return True
 
 status_dict = {200: 0, 301: 0, 400: 0, 401: 0,
                403: 0, 404: 0, 405: 0, 500: 0}
@@ -11,7 +49,7 @@ counter = 0
 try:
     for line in sys.stdin:
         tmp_list = line.split()
-        if (len(tmp_list) < 2):
+        if (validateData(line) is False):
             continue
         if int(tmp_list[-2]) in status_dict.keys():
             status_dict[int(tmp_list[-2])] += 1
