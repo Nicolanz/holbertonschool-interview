a03a0d209dd22beb67d40c3be53d44874b6df178
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-22 22:11:54 +0000

    Catching errors

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+else:
+    try:
+        num = eval(sys.argv[1])
+    except NameError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if (type(num) != int):
+        print("N must be a number")
+        sys.exit(1)
+    elif num < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    else:
+        print([[]])
