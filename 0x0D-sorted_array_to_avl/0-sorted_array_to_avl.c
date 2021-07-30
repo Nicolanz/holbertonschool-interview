e6d19eb1a2f530db8f56bbe4c7e701f69b42a29a
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-30 01:20:13 +0000

    AVL tree

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,50 +1,5 @@
 #include "binary_trees.h"
 /**
- * newnode - Creates new nodes
- * @value: value
- * Return: newnode
- */
-avl_t *newnode(int value)
-{
-	avl_t *newTreeNode;
-
-	newTreeNode = malloc(sizeof(avl_t));
-	if (newTreeNode == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		newTreeNode->n = value;
-		newTreeNode->parent = NULL;
-		newTreeNode->left = NULL;
-		newTreeNode->right = NULL;
-		return (newTreeNode);
-	}
-}
-/**
- * createTree - Function to create a tree
- * @array: array
- * @start: Start
- * @end: End
- * Return: newnode
- */
-avl_t *createTree(int *array, int start, int end)
-{
-	if (start > end)
-		return (NULL);
-
-	int mid = (start + end) / 2;
-	avl_t *root = newnode(array[mid]);
-
-	root->left = createTree(array, start, mid - 1);
-
-	root->right = createTree(array, mid + 1, end);
-
-	return (root);
-}
-
-/**
  * sorted_array_to_avl - Creates array from AVL
  * @array: array
  * @size: size
@@ -52,11 +7,21 @@ avl_t *createTree(int *array, int start, int end)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	int start = 0;
-	int end = size - 1;
+	size_t i = 0;
+	binary_tree_t *newTreeNode;
 
-	avl_t *root = createTree(array, start, end);
+	while (i < size)
+	{
+		newTreeNode = malloc(sizeof(binary_tree_t));
 
-	return (root);
+		if (newTreeNode == NULL)
+			return (NULL);
+
+		newTreeNode->parent = NULL;
+		newTreeNode->n = array[i];
+		newTreeNode->left = NULL;
+		newTreeNode->right = NULL;
+		i++;
+	}
+	return (newTreeNode);
 }
-
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -24,8 +24,7 @@ typedef struct binary_tree_s avl_t;
 
 
 avl_t *sorted_array_to_avl(int *array, size_t size);
-avl_t *newnode(int value);
-avl_t *createTree(int *array, int start, int end);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
