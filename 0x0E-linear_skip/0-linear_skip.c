5b99abfd638f421f02f4bdfc4e563ea7b1e6a8c2
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-06 04:36:56 +0000

    Linear skip

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,14 @@
+#include "search.h"
+/**
+* linear_skip - Linear skip function
+* @list: List to get values
+* @value: value
+*
+* Return: the new list
+*/
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node;
+
+	return (node);
+}
