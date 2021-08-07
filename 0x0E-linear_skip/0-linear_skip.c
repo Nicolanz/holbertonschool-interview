cacc318de92523473cd047aa0aff0888b3918cd0
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-07 01:11:57 +0000

    Linear Skip

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,7 +1,7 @@
 #include "search.h"
 /**
-* linear_skip - Linear skip function
-* @list: List to get values
+* linear_skip - Linear skip function to look for a value
+* @list: linked list to check
 * @value: value
 *
 * Return: the new list
@@ -9,6 +9,43 @@
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *node;
+	skiplist_t *tmp;
 
-	return (node);
+	node = list;
+
+	if (node == NULL)
+		return (NULL);
+
+	while (node->express)
+	{
+		tmp = node->express;
+		printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
+
+		if (value < tmp->n)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, tmp->index);
+			break;
+		}
+		node = tmp;
+		if (!node->express)
+		{
+			while (tmp->next)
+			{
+				tmp = tmp->next;
+			}
+			printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, tmp->index);
+		}
+	}
+
+	while (node != tmp)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+		if (node->n == value)
+			return (node);
+		node = node->next;
+	}
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	return (NULL);
 }
---
+++ 0x0E-linear_skip/README.md
@@ -0,0 +1,1 @@
+This is the folder to resolve the **0x0E. Linear search in skip list** project at Holberton School
