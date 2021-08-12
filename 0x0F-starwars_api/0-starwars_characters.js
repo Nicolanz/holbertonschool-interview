7b85608705692789bb25df55bf4415834ce3d149
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-12 23:59:46 +0000

    Star Wars API

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const filmUrl = 'https://swapi-api.hbtn.io/api/films/';
+const episode = parseInt(process.argv[2]);
+
+request(filmUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+
+  const json = JSON.parse(body);
+  const characters = json.results[episode - 1].characters;
+
+  for (const i in characters) {
+    request(characters[i], function (error, response, body) {
+      if (error) {
+        console.log(error);
+      }
+      const person = JSON.parse(body);
+      console.log(person.name);
+    });
+  }
+});
