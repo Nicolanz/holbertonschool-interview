6a5178c9b4271cc06482a96c2b150f088ab065f1
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-13 00:37:09 +0000

    Changing url

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,29 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
-const filmUrl = 'https://swapi-api.hbtn.io/api/films/';
-const episode = parseInt(process.argv[2]);
+const episode = process.argv[2];
+const filmUrl = 'https://swapi-api.hbtn.io/api/films/' + episode;
 
-if (episode > 0 && episode < 8) {
-  request(filmUrl, function (error, response, body) {
-    if (error) {
-      // pass
-    } else {
-      const json = JSON.parse(body);
-      const characters = json.results[episode - 1].characters;
+request(filmUrl, function (error, response, body) {
+  if (error) {
+    // pass
+  } else {
+    const json = JSON.parse(body);
+    const characters = json.characters;
 
-      for (const i in characters) {
-        request(characters[i], function (error, response, body) {
-          if (error) {
-            // pass
-          } else {
-            const person = JSON.parse(body);
-            console.log(person.name);
-          }
-        });
-      }
+    for (const i in characters) {
+      request(characters[i], function (error, response, body) {
+        if (error) {
+          // pass
+        } else {
+          const person = JSON.parse(body);
+          console.log(person.name);
+        }
+      });
     }
-  });
-} else {
-  // pass
-}
+  }
+});
