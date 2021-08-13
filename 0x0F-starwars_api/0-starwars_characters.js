ddb3b8277fa57ce605a734847041b685ca08eb5b
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-13 00:07:21 +0000

    Catch Errors

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,21 +4,25 @@ const request = require('request');
 const filmUrl = 'https://swapi-api.hbtn.io/api/films/';
 const episode = parseInt(process.argv[2]);
 
-request(filmUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
+if (episode > 0 && episode < 8) {
+  request(filmUrl, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
 
-  const json = JSON.parse(body);
-  const characters = json.results[episode - 1].characters;
+    const json = JSON.parse(body);
+    const characters = json.results[episode - 1].characters;
 
-  for (const i in characters) {
-    request(characters[i], function (error, response, body) {
-      if (error) {
-        console.log(error);
-      }
-      const person = JSON.parse(body);
-      console.log(person.name);
-    });
-  }
-});
+    for (const i in characters) {
+      request(characters[i], function (error, response, body) {
+        if (error) {
+          console.log(error);
+        }
+        const person = JSON.parse(body);
+        console.log(person.name);
+      });
+    }
+  });
+} else {
+  // pass
+}
