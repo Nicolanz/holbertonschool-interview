603eede8fa0ea6a848b3c532c6115d1f74c23033
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-13 00:30:46 +0000

    Catch more errors

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -7,20 +7,21 @@ const episode = parseInt(process.argv[2]);
 if (episode > 0 && episode < 8) {
   request(filmUrl, function (error, response, body) {
     if (error) {
-      console.log(error);
-    }
+      // pass
+    } else {
+      const json = JSON.parse(body);
+      const characters = json.results[episode - 1].characters;
 
-    const json = JSON.parse(body);
-    const characters = json.results[episode - 1].characters;
-
-    for (const i in characters) {
-      request(characters[i], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        }
-        const person = JSON.parse(body);
-        console.log(person.name);
-      });
+      for (const i in characters) {
+        request(characters[i], function (error, response, body) {
+          if (error) {
+            // pass
+          } else {
+            const person = JSON.parse(body);
+            console.log(person.name);
+          }
+        });
+      }
     }
   });
 } else {
