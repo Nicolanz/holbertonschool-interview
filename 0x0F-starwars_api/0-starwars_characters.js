63fe8d5b0e3f567264990d1c68d320ab55bd8892
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-13 02:04:51 +0000

    0-starwars

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,22 +4,29 @@ const request = require('request');
 const episode = process.argv[2];
 const filmUrl = 'https://swapi-api.hbtn.io/api/films/' + episode;
 
-request(filmUrl, function (error, response, body) {
-  if (error) {
-    // pass
-  } else {
-    const json = JSON.parse(body);
-    const characters = json.characters;
-
-    for (const i in characters) {
-      request(characters[i], function (error, response, body) {
-        if (error) {
-          // pass
-        } else {
-          const person = JSON.parse(body);
-          console.log(person.name);
-        }
-      });
+async function firstRequest (callback) {
+  await callback();
+}
+function secondRequest () {
+  request(filmUrl, function (error, response, body) {
+    if (error) {
+      // pass
+    } else {
+      const json = JSON.parse(body);
+      const characters = json.characters;
+      for (const i in characters) {
+        firstRequest(function () {
+          request(characters[i], async function (error, response, body) {
+            if (error) {
+              // pass
+            } else {
+              const person = JSON.parse(body);
+              console.log(person.name);
+            }
+          });
+        });
+      }
     }
-  }
-});
+  });
+}
+secondRequest();
