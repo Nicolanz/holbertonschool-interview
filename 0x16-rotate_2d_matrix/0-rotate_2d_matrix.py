a28b87bab5776738a7f0d03a5c0286fa377becac
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-30 23:40:47 +0000

    rotate matrix

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 """Module containing a function to rotate a matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Function to rotate the matrix"""
+    my_list = []
+    for i in range(len(matrix)):
+        new_list = []
+        for b in range(len(matrix)):
+            new_list.insert(0, matrix[b][i])
+        my_list.append(new_list)
+
+    for i in range(len(matrix)):
+        for b in range(len(matrix)):
+            matrix[i][b] = my_list[i][b]
+
+    return matrix
